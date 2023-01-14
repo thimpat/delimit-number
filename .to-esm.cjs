5dfeb44d524e03444c3c79378af7fb3f45b9a922
Patrice THIMOTHEE <thimpat@gmail.com>
2023-01-14 13:09:23 +0000

    CI: Update to-esm configuration

---
+++ .to-esm.cjs
@@ -0,0 +1,9 @@
+// Replacements for tests
+module.exports = {
+    "replaceStart"  : [
+        {
+            "search" : "From the .cjs module",
+            "replace": `From the .mjs module`
+        },
+    ],
+}
